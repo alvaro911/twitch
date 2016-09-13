af3a986e2252e29775707e1a55e6a15034a7a2e0
alvaro911 <algp232@hotmai.com>
2016-09-12 19:21:58 -0600

    using a factory to do an http request

--- dist/twitch.js
+++ dist/twitch.js
@@ -1,5 +1,35 @@
 (function(){
 	angular
-	.module('TwitchApp', [require,('angular-animate')])
-	.constant('dataUrl', 'https://api.twitch.tv/kraken/streams/');
-}(angular));
+	.module('TwitchApp', ['ngAnimate'])
+	.factory('twitchService', ['$http', twitchService])
+	.controller('TwitchCtrl', ['twitchService', '$log' ,twitchFn]);
+
+	function twitchFn(channel, $log){
+		var self = this;
+		var channels = ['adobe', 'freecodecamp', 'legendsofgaminguk'];
+		self.users = [];
+		var getChannels = function(response){
+			self.users.push(response.data);
+			$log.info(response);
+		};
+		var requests = channel(channels);
+		requests.forEach(function(req){
+			req.then(getChannels);
+		});
+	}
+
+	function twitchService($http){
+		var channel = function(chan){
+			var requests = [];
+			for(var i=0; i<chan.length; i++){
+				var req = $http({
+					method:'GET',
+					url:'https://api.twitch.tv/kraken/channels/' + chan[i]
+				});
+				requests.push(req);
+			}
+			return requests;
+		};
+		return channel;
+	}
+}());
